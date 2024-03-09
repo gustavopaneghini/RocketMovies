2c75de5e70858bd1fbf2befb3d1bb311e4ddb224
Gustavo Paneghini <gustavogpaneghini@hotmail.com>
2024-03-09 12:19:08 -0300

    adicionado ; para padronizacao do codigo

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -2,11 +2,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import { GlobalStyles } from './public/global.js'
+import { GlobalStyles } from './public/global.js';
 
 import { Details } from "./pages/Details/index.jsx";
 import { Profile } from "./pages/Profile";
-import { SignIn } from './pages/SignIn'
+import { SignIn } from './pages/SignIn/index.jsx';
+import { SignUp } from './pages/SignUp/index.jsx';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
